b78a2a8c2c84bee7be39a836c9e8b12da1c3ab15
Gionnelly <80008338+gvielza@users.noreply.github.com>
2022-04-13 22:13:49 -0300

    instalando y configurando taiwindcss

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Formulario extends Component {
+  render() {
+    return (
+      <h1 >Formulario</h1>
+    )
+  }
+}
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ListadoPacientes extends Component {
+  render() {
+    return (
+      <>
+          <h2>Listado Pacientes</h2>
+          
+          </>
+    )
+  }
+}
